0d7aca710564d40e24d1a0caa633dc5b85ce8bca
Evkasonka <121517773+Evkasonka@users.noreply.github.com>
2023-09-05 01:42:31 +0300

    16final_after_2review

--- backend/users/views.py
+++ backend/users/views.py
@@ -3,7 +3,8 @@ from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import (IsAuthenticated,
+                                        IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
 
 from djoser.views import UserViewSet
